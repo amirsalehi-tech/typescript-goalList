fadecd4e0647817c787b4b7f6ed90b3556bb1fb4
Amir2292 <amiamirsalehi@gmail.com>
2025-04-16 14:32:55 +0330

    add courseGoal component and define it's types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import CourseGoal from "./components/CourseGoal";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <CourseGoal title="Learn React" description="Master React" />
+    </main>
+  );
 }
